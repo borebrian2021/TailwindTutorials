9c42946be3f91e21ecdcaf4084459fb89448eba5
borebrian2021 <bkimutai2021@gmail.com>
2022-11-15 22:27:30 +0300

    Before rollback

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div>
       <nav>
         <div>
-          <h1>
+          <h1 className='uppercase p-4 border-b border-gray-100'>
             <a href="/">Food Ninja</a>
           </h1>
         </div>
@@ -32,21 +32,21 @@ function App() {
       </nav>
     </div>
   
-    <main>
-      <div>
+    <main className='px-16 py-6'>
+      <div >
         <a href="#">Log in</a>
         <a href="#">Sign up</a>
       </div>
 
       <header>
         <h2 className="text-6xl font-semibold">Recipes</h2>
-        <h3 className="font-bold font-semibold uppercase">For Ninjas</h3>
+        <h3 className="font-semibold uppercase">For Ninjas</h3>
       </header>
 
       <div>
-        <h4>Latest Recipes</h4>
+        <h4 className='mt-12 pb-2 border-b border-gray-200'>Latest Recipes</h4>
   
-        <div>
+        <div className='mt-8'>
           <div> 
             <img src="https://www.thespruceeats.com/thmb/OnwvqaCO9b1qp1u322XW4TZAIkE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/instant-pot-beef-stew-5218740-hero-05-ee714d6ec6ca45c2aef9a83181944c3d.jpg" alt="stew"/>
             <div>
@@ -56,9 +56,9 @@ function App() {
           </div>
         </div>
 
-        <h4>Most Popular</h4>
+        <h4 className='mt-12 pt-2 border-b border-gray-200'>Most Popular</h4>
 
-        <div>
+        <div className='mt-8'>
         </div>
       </div>
 
